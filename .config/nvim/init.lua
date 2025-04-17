f54b94c0c997d1486f3602b3f86371c51d3dbf82
Jiwoo Ahn <ilp_rincipe@naver.com>
2025-04-17 10:29:24 +0900

    Update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- Lazy.nvim setup
-vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
      -- LSP configuration
     { "neovim/nvim-lspconfig" },
@@ -14,17 +15,12 @@ require("lazy").setup({
     { "saadparwaiz1/cmp_luasnip" },
 
     -- Formatter and diagnostics
-    { "jose-elias-alvarez/null-ls.nvim" },
     { "nvim-lua/plenary.nvim" },
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim"},
-    { "neovim/nvim-lspconfig"},
 
     -- Treesitter for syntax highlighting
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-
-    -- File explorer
-    { "nvim-tree/nvim-tree.lua" },
 
     -- Telescope for fuzzy searching
     { "nvim-telescope/telescope.nvim" },
@@ -36,20 +32,50 @@ require("lazy").setup({
     { "nvim-lualine/lualine.nvim" },
 })
 
--- Setup LSP (clangd for C++)
+-- Setup LSP
 local lspconfig = require("lspconfig")
+
+local function on_attach(client, bufnr)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    -- LSP keymap
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+
+    --  Formatting keymap
+    vim.keymap.set("n", "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+    end, { noremap = true, silent = true, buffer = bufnr, desc = "Format Buffer" })
+end
+
+-- clangd setup
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 lspconfig.clangd.setup({
     cmd = { "clangd", "--background-index", "--clang-tidy" },
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
-    on_attach = function(_, bufnr)
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-    end,
+    root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
+    capabilities = capabilities,
+    on_attach = on_attach,
 })
+
+-- pyright setup
+lspconfig.pyright.setup({
+    settings = {
+        python = {
+            analysis = {
+                typeCheckingMode = "basic",
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+            },
+        },
+    },
+    on_attach = on_attach,
+    capabilities = capabilities,	
+})
+
 
 -- Auto-completion setup
 local cmp = require("cmp")
@@ -74,43 +100,22 @@ cmp.setup({
     }),
 })
 
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "clangd", "pyright" },
+	automatic_installation = true,
+})
+
 -- Treesitter setup
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "cpp", "c", "lua" },
+    ensure_installed = { "cpp", "c", "lua", "python" },
     highlight = { enable = true },
-	autopair = { },
     indent = { enable = true },
 })
 
 -- Telescope setup
 require("telescope").setup()
-
--- Mason setup
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-
--- Null-ls setup
-local null_ls = require("null-ls")
-null_ls.setup({
-    sources = {
-        -- Formatters
-        null_ls.builtins.formatting.clang_format,  -- C/C++
-        null_ls.builtins.formatting.prettier,     -- JavaScript, TypeScript, etc.
-
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_buf_set_keymap(
-                bufnr,
-                "n",
-                "<leader>f",
-                "<cmd>lua vim.lsp.buf.format({ async = true })<CR>",
-                { noremap = true, silent = true, desc = "Format Buffer" }
-            )
-        end
-    end,
-})
 
 -- keymap
 vim.g.mapleader = " "
@@ -118,15 +123,22 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "-", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
-vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.format({ async = true })
-end, { desc = "Format Buffer" })
 
 -- General Vim commands
 vim.wo.number = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.cursorline = true
 
-vim.cmd('set clipboard=unnamed')
+vim.opt.clipboard = "unnamedplus"
 
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
+
+vim.cmd.colorscheme("default")
