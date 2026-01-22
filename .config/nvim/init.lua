6ce5c31485ae38acc4bfa0c6c4e69ef8baa2931f
ilp-sys <ilp_rincipe@naver.com>
2026-01-22 13:45:06 +0900

    Add keymap for LSP references in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -231,6 +231,7 @@ vim.lsp.enable('pyright')
 vim.lsp.enable('gopls')
 
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
 vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
 
